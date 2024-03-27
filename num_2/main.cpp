f8a5b8bbd1e7ccef5321148fd103ba9651459bdf
viktshevchenk <viktoriashevchenk@yandex.ru>
2024-03-27 17:18:58 +0300

    add methods inversion, conjunction, disjunction, implication, coimplication

--- num_2/main.cpp
+++ num_2/main.cpp
@@ -5,10 +5,11 @@ class logical_values_array {
 public:
     logical_values_array(unsigned int value = 0) { _value = value; }
     void accessor(unsigned int value) { _value = value; }
-
-
-
-
+    void inversion();
+    logical_values_array conjunction(logical_values_array ob2) const;
+    logical_values_array disjunction(logical_values_array ob2) const;
+    logical_values_array implication(logical_values_array ob2) const;
+    logical_values_array coimplication(logical_values_array ob) const;
 
 
 
@@ -16,7 +17,39 @@ public:
     void convertation(char* &ob);
 };
 
+
+void logical_values_array::inversion() {
+    (_value > 0)?(_value = 0):(_value = 1);
+}
+
+logical_values_array logical_values_array::conjunction(logical_values_array ob2) const {
+    logical_values_array temp;
+    (_value > 0 && ob2._value > 0)?(temp._value = 1):(temp._value = 0);
+    return temp;
+}
+
+logical_values_array logical_values_array::disjunction(logical_values_array ob2) const {
+    logical_values_array temp;
+    (_value > 0 || ob2._value > 0)?(temp._value = 1):(temp._value = 0);
+    return temp;
+}
+
+logical_values_array logical_values_array::implication(logical_values_array ob2) const {
+    logical_values_array temp;
+    (_value > 0 && ob2._value == 0)?(temp._value = 0):(temp._value = 1);
+    return temp;
+}
+
+logical_values_array logical_values_array::coimplication(logical_values_array ob2) const {
+    logical_values_array temp;
+    //(_value > 0 && ob2._value == 0)?(temp._value = 1):(temp._value = 0);
+    temp.implication(ob2);
+    temp.inversion();
+    return temp;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    logical_values_array ob1(1), ob2(0);
+    ob1.coimplication(ob2);
     return 0;
 }
