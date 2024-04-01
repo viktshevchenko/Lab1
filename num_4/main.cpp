49c0e31e50d31adf169213e6d9a77c3dd4cb3ac6
viktshevchenko <163758752+viktshevchenko@users.noreply.github.com>
2024-04-01 23:46:16 +0300

    finish doing num_4

--- num_4/main.cpp
+++ num_4/main.cpp
@@ -1,25 +1,273 @@
 #include <iostream>
+#include <cstdarg>
+#include <cmath>
+#include <iomanip>
 
 class matrix {
     double** element;
     size_t _size;
+
 public:
-    matrix(size_t size, double args, ...);
+    matrix(size_t size, unsigned int args, ...);
+    matrix() { element = nullptr; _size = 0; }
     matrix(matrix const &ob);
     ~matrix();
-    matrix &operator=(matrix &ob2);
-    matrix &operator*(matrix const ob2);
-    friend matrix& operator*(matrix ob, double num);
-    friend matrix& operator*(double num, matrix ob);
-    matrix &operator+(matrix const ob2);
-    matrix &operator-(matrix const ob2);
+    matrix & operator=(matrix const &ob2);
+    matrix operator*(matrix const ob2);
+    friend matrix operator*(matrix ob, double num);
+    friend matrix operator*(double num, matrix ob);
+    matrix operator+(matrix const ob2);
+    matrix operator-(matrix const ob2);
     double determinant();
-    matrix transponir();
-    matrix inverse();
-    double operator[](int index);
+    void transponir();
+    void inverse(matrix &result);
+    void obr(matrix &newMat);
+    void show();
+
+    class Proxy {
+        double* _array;
+    public:
+        Proxy(double* _array) : _array(_array) { }
+
+        double operator[](int index) {
+            return _array[index];
+        }
+    };
+
+    Proxy operator[](int index) {
+        return Proxy(element[index]);
+    }
 };
 
+matrix::matrix(size_t size, unsigned int args, ...) {
+    _size = size;
+    va_list va;
+    va_start(va, args);
+    element = new double *[size];
+    for(size_t i = 0; i < size; i++) {
+        element[i] = new double[size];
+        for(size_t j = 0; j < size; j++) {
+            if(args > 0){
+                element[i][j] = va_arg(va, double);
+                args--;
+            }
+            else
+                element[i][j] = 0.0;
+        }
+    }
+    va_end(va);
+}
+
+matrix::matrix(matrix const &ob) {
+    _size = ob._size;
+    element = new double *[_size];
+    for(size_t i = 0; i < _size; i++) {
+        element[i] = new double[_size];
+        for(size_t j = 0; j < _size; j++) {
+            element[i][j] = ob.element[i][j];
+        }
+    }
+}
+
+matrix::~matrix() {
+    for(size_t i = 0; i < _size; i++) delete[] element[i];
+    delete[] element;
+}
+
+matrix & matrix::operator=(matrix const &ob2) {
+    if(&ob2 != this) {
+        this->~matrix();
+        _size = ob2._size;
+        element = new double *[_size];
+        for(size_t i = 0; i < _size; i++) {
+            element[i] = new double[_size];
+            for(size_t j = 0; j < _size; j++) {
+                element[i][j] = ob2.element[i][j];
+            }
+        }
+    }
+    return *this;
+}
+
+matrix matrix::operator+(matrix const ob2) {
+    if(_size != ob2._size) return *this;
+    matrix temp = *this;
+    for(size_t i = 0; i < _size; i++) {
+        for(size_t j = 0; j < _size; j++)
+            temp.element[i][j] += ob2.element[i][j];
+    }
+    return temp;
+}
+
+matrix matrix::operator-(matrix const ob2) {
+    if(_size != ob2._size) return *this;
+    matrix temp = *this;
+    for(size_t i = 0; i < _size; i++) {
+        for(size_t j = 0; j < _size; j++)
+            temp.element[i][j] -= ob2.element[i][j];
+    }
+    return temp;
+}
+
+matrix matrix::operator*(matrix const ob2) {
+    matrix temp = *this;
+    double value;
+    for(size_t i = 0; i < _size; i++) {
+        for(size_t j = 0; j < _size; j++) {
+            value = 0.0;
+            for(size_t k = 0; k < _size; k++) {
+                value += element[i][k] * ob2.element[k][j];
+            }
+            temp.element[i][j] = value;
+        }
+    }
+    return temp;
+}
+
+matrix operator*(matrix ob, double num) {
+    matrix temp = ob;
+    for(size_t i = 0; i < ob._size; i++) {
+        for(size_t j = 0; j < ob._size; j++) {
+            temp.element[i][j] *= num;
+        }
+    }
+    return temp;
+}
+
+matrix operator*(double num, matrix ob) {
+    matrix temp = ob;
+    for(size_t i = 0; i < ob._size; i++) {
+        for(size_t j = 0; j < ob._size; j++) {
+            temp.element[i][j] *= num;
+        }
+    }
+    return temp;
+}
+
+double matrix::determinant() {
+    matrix temp = *this;
+    size_t n = temp._size;
+    double det = 1.0;
+
+    for (size_t i = 0; i < n; ++i) {
+
+        double pivotElement = temp.element[i][i];
+        size_t pivotRow = i;
+        for (size_t row = i + 1; row < n; ++row) {
+            if (std::abs(temp.element[row][i]) > std::abs(pivotElement)) {
+                pivotElement = temp.element[row][i];
+                pivotRow = row;
+            }
+        }
+        if (pivotElement == 0.0)
+            return 0.0;
+
+        if (pivotRow != i) {
+            std::swap(temp.element[i], temp.element[pivotRow]);
+            det *= -1.0;
+        }
+        det *= pivotElement;
+
+        for (size_t row = i + 1; row < n; ++row) {
+            for (size_t col = i + 1; col < n; ++col)
+                temp.element[row][col] -= temp.element[row][i] * temp.element[i][col] / pivotElement;
+        }
+    }
+    return det;
+}
+
+void matrix::transponir() {
+    matrix temp = *this;
+    for(size_t i = 0; i < temp._size; i++){
+        for(size_t j = 0; j < temp._size; j++){
+            temp.element[i][j] = this->element[j][i];
+        }
+    }
+    *this = temp;
+}
+
+void matrix::inverse(matrix &newMat){
+    if(determinant() == 0.0)  return;
+    if(_size != newMat._size) return;
+    matrix mat = *this;
+    size_t prom = 0;
+    double dprom = 0;
+    for(size_t i = 0; i < mat._size; i++) {
+        if (mat.element[i][i] == 0) {
+            prom = i;
+            while (mat.element[prom][i] != 0) {
+                prom++;
+                if (prom == mat._size) return;
+            }
+            for (size_t j = i; j < mat._size; j++) {
+                std::swap(mat.element[prom][j], mat.element[i][j]);
+                std::swap(newMat.element[prom][j], newMat.element[i][j]);
+            }
+        }
+        dprom = mat.element[i][i];
+        for(size_t j = 0; j < mat._size; j++){
+            mat.element[i][j] /= dprom;
+            newMat.element[i][j] /= dprom;
+        }
+        for(size_t j = i+1; j < mat._size; j++){
+            dprom = mat.element[j][i];
+            for(size_t k = 0; k < mat._size; k++){
+                mat.element[j][k] -= mat.element[i][k]*dprom;
+                newMat.element[j][k] -= newMat.element[i][k]*dprom;
+            }
+        }
+    }
+    for(long long int i = mat._size-1; i >= 0; i--){
+        for(long long int j = i-1; j >= 0; j--){
+            dprom = mat.element[j][i];
+            for(size_t k = 0; k < mat._size; k++){
+                mat.element[j][k] -= mat.element[i][k]*dprom;
+                newMat.element[j][k] -= newMat.element[i][k]*dprom;
+            }
+        }
+    }
+}
+
+
+void matrix::show() {
+    for(size_t i = 0; i < _size; i++) {
+        for(size_t j = 0; j < _size; j++) {
+            std::cout << std::showpoint << std::left << std::setw(10) << element[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    printf("\n\n");
+}
+
 int main() {
+    matrix m1(3, 9, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0), m2(3, 1, 2.0), m3(2, 0);
+    matrix m4(3, 5, 4, 3, 2, 1, 0), m5(3, 9, 3.0, 3.0, -1.0, 4.0, 1.0, 3.0, 1.0, -2.0, -2.0),
+    m6(2, 4, 1, 2, 3, 4);
+
+    m3 = m1 + m2;
+    m3.show();
+
+    m3 = m1 - m2;
+    m3.show();
+
+    m3 = m1 * 2;
+    m3.show();
+
+    m3 = 5 * m1;
+    m3.show();
+
+    m3 = m1 * m4;
+    m3.show();
+
+    std::cout << m5.determinant() << std::endl << std::endl;
+
+    m1.transponir();
+    m1.show();
+
+    m5.inverse(m3);
+    m3.show();
+
+    std::cout << m1[0][2] << std::endl;
 
     return 0;
 }
