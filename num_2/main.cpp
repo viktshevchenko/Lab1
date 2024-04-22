a7ec2b8c0f375fcbfe93275d97e7386a59b33d3c
viktshevchenko <163758752+viktshevchenko@users.noreply.github.com>
2024-04-22 23:16:50 +0300

    amendments have been made and fixed a bug with a memory leak

--- num_2/main.cpp
+++ num_2/main.cpp
@@ -6,8 +6,8 @@ char* dec_to_bin(int num);
 class logical_values_array {
     unsigned int _value;
 public:
-    logical_values_array(unsigned int value = 0) { _value = value; }
-    void accessor(unsigned int value) { _value = value; }
+    logical_values_array(unsigned int value = 0);
+    void accessor(unsigned int value);
     void inversion();
     logical_values_array operator=(logical_values_array ob2);
     logical_values_array conjunction(logical_values_array ob2) const;
@@ -18,19 +18,24 @@ public:
     logical_values_array equivalence(logical_values_array ob2) const;
     logical_values_array pier_arrow(logical_values_array ob2) const;
     logical_values_array stroke_schaeffer(logical_values_array ob2) const;
-    static bool equals(unsigned int ob1, unsigned int ob2) { return ob1 == ob2; }
+    static bool equals(unsigned int ob1, unsigned int ob2);
     bool get_bit(unsigned int position);
-    void position(char* &p) { p = strdup(dec_to_bin(_value)); }
-    void show(int num) { std::cout << "value" << num << ": " << _value << std::endl; }
-    void show() { std::cout << _value << std::endl; }
-    unsigned int get() { return _value; }
+    void position(char* &p) const;
+    unsigned int get() const;
 };
+
+logical_values_array::logical_values_array(unsigned int value) {
+    _value = value;
+}
+
+void logical_values_array::accessor(unsigned int value) {
+    _value = value;
+}
 
 logical_values_array logical_values_array::operator=(logical_values_array ob2) {
     _value = ob2._value;
     return *this;
 }
-
 
 void logical_values_array::inversion() {
     (_value > 0)?(_value = 0):(_value = 1);
@@ -93,7 +98,7 @@ logical_values_array logical_values_array::stroke_schaeffer(logical_values_array
 char* dec_to_bin(int num) {
     int k;
     char* buf = new char [sizeof(unsigned int) + 1];
-    char *p = buf + sizeof(unsigned int);
+    char *p = buf + sizeof(unsigned int), *temp;
     buf[sizeof(unsigned int) + 1] = '\0';
     while(num != 0) {
         k = num & 0x00000001;
@@ -101,7 +106,13 @@ char* dec_to_bin(int num) {
         p--;
         num = num >> 1;
     }
-    return ++p;
+    temp = strdup(++p);
+    delete [] buf;
+    return temp;
+}
+
+bool logical_values_array::equals(unsigned int ob1, unsigned int ob2) {
+    return ob1 == ob2;
 }
 
 bool logical_values_array::get_bit(unsigned int position) {
@@ -113,7 +124,24 @@ bool logical_values_array::get_bit(unsigned int position) {
         return res;
     }
     res = p[position - 1] - '0';
+    delete [] p;
     return res;
+}
+
+void logical_values_array::position(char* &p) const {
+    p = strdup(dec_to_bin(_value));
+}
+
+unsigned int logical_values_array::get() const {
+    return _value;
+}
+
+void show(int num, logical_values_array ob) {
+    std::cout << "value" << num << ": " << ob.get() << std::endl;
+}
+
+void show(logical_values_array ob) {
+    std::cout << ob.get() << std::endl;
 }
 
 int main() {
@@ -123,102 +151,102 @@ int main() {
 
     // accessor
     std::cout << "accessor:" << std::endl;
-    ob2.show();
+    show(ob2);
     ob2.accessor(3);
-    ob2.show();
+    show(ob2);
     std::cout << "----------" << std::endl;
 
     // inversion
-    ob1.show(1);
+    show(1, ob1);
     std::cout << "inversion: ";
     ob1.inversion();
-    ob1.show();
+    show(ob1);
     std::cout << "----------" << std::endl;
 
     // conjunction
-    ob1.show(1);
-    ob2.show(2);
+    show(1, ob1);
+    show(2, ob2);
     ob3 = ob1.conjunction(ob2);
     std::cout << "conjunction: ";
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // disjunction
-    ob1.show(1);
-    ob2.show(2);
+    show(1, ob1);
+    show(2, ob2);
     ob3 = ob1.disjunction(ob2);
     std::cout << "disjunction: ";
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // implication
-    ob1.show(1);
-    ob4.show(2);
+    show(1, ob1);
+    show(2, ob4);
     std::cout << "implication (v1->v2): ";
     ob3 = ob1.implication(ob4);
-    ob3.show();
+    show(ob3);
     std::cout << "implication (v2->v1): ";
     ob3 = ob4.implication(ob1);
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // coimplication
-    ob1.show(1);
-    ob4.show(2);
+    show(1, ob1);
+    show(2, ob4);
     std::cout << "coimplication !(v1->v2): ";
     ob3 = ob1.coimplication(ob4);
-    ob3.show();
+    show(ob3);
     std::cout << "coimplication !(v2->v1): ";
     ob3 = ob4.coimplication(ob1);
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // addition mod 2
-    ob1.show(1);
-    ob2.show(2);
-    ob5.show(3);
+    show(1, ob1);
+    show(2, ob2);
+    show(3, ob5);
     ob3 = ob1.addition_mod_2(ob2);
     std::cout << "addition mod 2 (v1 v2): ";
-    ob3.show();
+    show(ob3);
     ob3 = ob1.addition_mod_2(ob5);
     std::cout << "addition mod 2 (v1 v3): ";
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // equivalence
-    ob1.show(1);
-    ob2.show(2);
-    ob5.show(3);
+    show(1, ob1);
+    show(2, ob2);
+    show(3, ob5);
     ob3 = ob1.equivalence(ob2);
     std::cout << "equivalence (v1~v2): ";
-    ob3.show();
+    show(ob3);
     ob3 = ob1.equivalence(ob5);
     std::cout << "equivalence (v1~v3): ";
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // Pier arrow
-    ob1.show(1);
-    ob2.show(2);
-    ob5.show(3);
+    show(1, ob1);
+    show(2, ob2);
+    show(3, ob5);
     ob3 = ob1.pier_arrow(ob2);
     std::cout << "Pier arrow (v1 v2): ";
-    ob3.show();
+    show(ob3);
     ob3 = ob1.pier_arrow(ob5);
     std::cout << "Pier arrow (v1 v3): ";
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // stroke Schaeffer
-    ob1.show(1);
-    ob2.show(2);
-    ob4.show(3);
+    show(1, ob1);
+    show(2, ob2);
+    show(3, ob4);
     ob3 = ob1.stroke_schaeffer(ob2);
     std::cout << "stroke Schaeffer (v1 | v2): ";
-    ob3.show();
+    show(ob3);
     ob3 = ob2.stroke_schaeffer(ob4);
     std::cout << "stroke Schaeffer (v2 | v3): ";
-    ob3.show();
+    show(ob3);
     std::cout << "----------" << std::endl;
 
     // get_bit()
@@ -229,7 +257,7 @@ int main() {
     // position
     char *p = new char [20];
     ob6.position(p);
-    std::cout << "position(): " << p << std::endl;
+    std::cout << p << std::endl;
     delete [] p;
 
     return 0;
